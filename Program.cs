481e67d94620c3f64f2e35f8ebc4c4b79467ba59
Brenda Gonçalves <brenda.goncalves965@gmail.com>
2022-01-27 21:20:17 -0300

    add files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using jogo_rpg.src.Entities;
+
+namespace jogo_rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+            
+            
+            Console.WriteLine($"{wizard.Attack(10)}");
+            Console.WriteLine($"{wizard.Attack(1)}");
+        }
+    }
+}
